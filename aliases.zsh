f4318c3e087d8fe0a63f300957d877f8cafa4a5e
Craig Potter <craigpotter@live.com>
2023-01-15 07:03:05 +0000

    new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -6,15 +6,11 @@ alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-d
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
-alias compile="commit 'compile'"
-alias version="commit 'version'"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
-alias docs="lara && cd docs/"
+alias dev="cd $HOME/code"
 
 # Laravel
 alias a="php artisan"
@@ -22,6 +18,10 @@ alias fresh="php artisan migrate:fresh --seed"
 alias tinker="php artisan tinker"
 alias seed="php artisan db:seed"
 alias serve="php artisan serve"
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias sa='sail artisan'
+alias test='a test'
+alias sail-test 'sail artisan test'
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -54,3 +54,26 @@ alias resolve="git add . && git commit --no-edit"
 alias stash="git stash -u"
 alias unstage="git restore --staged ."
 alias wip="commit wip"
+
+# Mac
+alias o="open ."
+alias hostfile="sudo vim /etc/hosts"
+alias sshconfig="vi ~/.ssh/config"
+alias github="cd $HOME/code/github"
+alias bitbucket="cd $HOME/code/bitbucket"
+alias me="cd $HOME/code/github/craigpotter"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# IP addresses
+alias ip="curl https://diagnostic.opendns.com/myip ; echo"
+alias localip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Lock the screen
+alias afk="osascript -e 'tell application \"System Events\" to keystroke \"q\" using {command down,control down}'"
+
+# Empty the Trash on all mounted volumes and the main HDD
+# Also, clear Apple’s System Logs to improve shell startup speed
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
